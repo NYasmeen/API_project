13a0e070e90b3a7fe838f1e9371b70ecce82e8e1
NYasmeen <38100781+NYasmeen@users.noreply.github.com>
2018-09-05 07:04:44 +0100

    API project

--- routes/index.js
+++ routes/index.js
@@ -4,36 +4,25 @@ const axios = require('axios');
 
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
 
- var callapi = function() {
+  var callapi = function () {
     var updated = []
-    
     var url = 'https://jsonplaceholder.typicode.com/photos';
-    
+
     axios.get(url)
       .then(function (response) {
-        // console.log(response.data);
         updated = response.data
-        a=Math.floor((Math.random() * updated.length) + 1);
+        a = Math.floor((Math.random() * updated.length) + 1);
         console.log(a);
-        quote =  updated[a].title;
-        res.render('index', { title: quote});
-         
-        // for(let a = 0; a < updated.length; a++){
-           
-        // }
+        quote = updated[a].title;
+        res.render('index', { title: quote });
       })
       .catch(function (error) {
         console.log(error);
       })
 
-}();
+  }();
 });
-
-router.post('/', function(req, res, next) {
-
-});
-
 
 module.exports = router;
